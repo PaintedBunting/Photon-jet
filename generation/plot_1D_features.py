e7b358d5624e28261502523e2eecb51c03e9424a
PaintedBunting <85972504+PaintedBunting@users.noreply.github.com>
2021-08-05 13:13:32 -0700

    Update plot_1D_features.py
    Changed it to work on Python3.

--- generation/plot_1D_features.py
+++ generation/plot_1D_features.py
@@ -41,24 +41,21 @@ def get_x(myindex,ybin,zbin):
     if (myindex >=504):
         return -1
     elif (zbin==0):
-        return (myindex - ybin)/nbins1y
+        return (myindex - ybin)//nbins1y
     elif (zbin==1):
-        return (myindex - lvl1 - ybin)/nbins2y
+        return (myindex - lvl1 - ybin)//nbins2y
     else:
-        return (myindex - lvl1 - lvl2 - ybin)/nbins3y
+        return (myindex - lvl1 - lvl2 - ybin)//nbins3y
     pass
 
-myfile = TFile("cpion-10GeV-1k.root")
-#pion-10GeV-1k.root
-#electrons-10GeV-5k.root 
-#photons-10GeV-1k.root
+myfile = TFile("axion1_100GeV_20k.root")
 mytree = myfile.Get("fancy_tree")
 
 c = TCanvas("a","a",500,500)
 zsegmentation = TH1F("","",3,np.array([-240.,-150.,197.,240.]))
-sampling1_eta = TH2F("","",3,-240.,240.,480/5,-240.,240.)
-sampling2_eta = TH2F("","",480/40,-240.,240.,480/40,-240.,240.)
-sampling3_eta = TH2F("","",480/40,-240.,240.,480/80,-240.,240.)
+sampling1_eta = TH2F("","",3,-240.,240.,480//5,-240.,240.)
+sampling2_eta = TH2F("","",480//40,-240.,240.,480//40,-240.,240.)
+sampling3_eta = TH2F("","",480//40,-240.,240.,480//80,-240.,240.)
 
 Fraction_in_thirdlayer = TH1F("","",100,0,0.01)
 Fraction_not_in = TH1F("","",100,0,0.01)
@@ -66,14 +63,16 @@ Middle_lateral_width = TH1F("","",100,0,100)
 Front_lateral_width = TH1F("","",100,0,100)
 Shower_Depth = TH1F("","",100,0,1.5)
 Shower_Depth_width = TH1F("","",100,0,1.0)
+thingy = 0.
 
 for i in range(min(1000,mytree.GetEntries())):
     mytree.GetEntry(i)
     if (i%100==0):
-        print i,mytree.GetEntries()
+        print(i,mytree.GetEntries())
         pass
     y = "energy"
     exec("%s = %s" % (y,"mytree.cell_0"))
+    
     total_energy = 0.
     third_layer = 0.
     not_in = 0.
@@ -85,8 +84,10 @@ for i in range(min(1000,mytree.GetEntries())):
     first_layer_X2 = 0.
     front_energy = 0.
     middle_energy = 0.
+    
     for j in range(507):
         exec("%s = %s" % (y,"mytree.cell_"+str(j)))
+        
         xbin = get_x(j,get_y(j,get_z(j)),get_z(j))
         ybin = get_y(j,get_z(j))
         zbin = get_z(j)
@@ -95,8 +96,8 @@ for i in range(min(1000,mytree.GetEntries())):
         yvalue = 0.;
         xvalue = 0.;
         total_energy+=energy
-        lateral_depth+=zbin*energy
-        lateral_depth2+=zbin*zbin*energy
+        lateral_depth+= zbin*energy
+        lateral_depth2+= zbin*zbin*energy
         if (zbin==2):
             third_layer+=energy
             pass
@@ -138,30 +139,37 @@ for i in range(min(1000,mytree.GetEntries())):
     Shower_Depth_width.Fill((lateral_depth2/total_energy - (lateral_depth/total_energy)**2)**0.5)
     pass
 
-zsegmentation.Draw()
-c.Print("plots/tot_zprofil.pdf")
-sampling1_eta.Draw("colz")
-c.Print("plots/tot_xy_layer1.pdf")
-sampling2_eta.Draw("colz")
-c.Print("plots/tot_xy_layer2.pdf")
-gPad.SetLogz()
-sampling3_eta.Draw("colz")
-for i in range(1,sampling3_eta.GetNbinsX()+1):
-    for j in range(1,sampling3_eta.GetNbinsY()+1):
-        print i,j,sampling3_eta.GetBinContent(i,j)
-        pass
-    pass
-c.Print("plots/tot_xy_layer3.pdf")
-gPad.SetLogz(0)
-Fraction_in_thirdlayer.Draw()
-c.Print("plots/Fraction_in_thirdlayer.pdf")
-Fraction_not_in.Draw()
-c.Print("plots/Fraction_not_in.pdf")
-Shower_Depth.Draw()
-c.Print("plots/Shower_Depth.pdf")
-Middle_lateral_width.Draw()
-c.Print("plots/Middle_lateral_width.pdf")
-Front_lateral_width.Draw()
-c.Print("plots/Front_lateral_width.pdf")
-Shower_Depth_width.Draw()
-c.Print("plots/Shower_Depth_width.pdf")
+#just uncomment whichever feature you want to graph
+
+#zsegmentation.Draw()
+
+#gPad.SetLogz() #uncomment this if you want to see any of the following features
+
+#sampling1_eta.Draw("colz")
+
+#sampling2_eta.Draw("colz")
+
+#sampling3_eta.Draw("colz")
+#for i in range(1,sampling3_eta.GetNbinsX()+1):
+    #for j in range(1,sampling3_eta.GetNbinsY()+1):
+        #print(i,j,sampling3_eta.GetBinContent(i,j))
+        #pass
+    #pass
+    
+gPad.SetLogz(0) #uncomment this if you want to see any of the following features
+
+#Fraction_in_thirdlayer.Draw() 
+
+#Fraction_not_in.Draw() 
+
+#Shower_Depth.Draw() 
+
+#Middle_lateral_width.Draw() 
+
+#Front_lateral_width.Draw() 
+
+Shower_Depth_width.Draw() 
+
+
+c.Update()
+c.Draw()
